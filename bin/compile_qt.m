5ca3eada72180b473387ea96cd9b468eb8043649
Giuliano Casale <g.casale@imperial.ac.uk>
2017-03-14 21:21:35 +0000

    QT MATLAB 2016a release

--- bin/compile_qt.m
+++ bin/compile_qt.m
@@ -1,10 +1,14 @@
-%cd ..
-mcc -m qt_gen_main.m -a ./lib/jsonlab-1.5/* -a ./lib/m3a/*
-%cd bin/
+curdir=pwd;
+cd ../qt-gen/
+mcc -m qt_gen_main.m -a ./lib/jsonlab/* -a ./lib/m3a/* -a ./lib/kpc-toolbox/*  -a ./lib/map/*  -a ./lib/map/*  -a ./lib/mmpp/*  -a ./lib/mtrace/*  -a ./lib/DERIVESTsuite/*
 if ispc
-movefile readme.txt readme-win.txt
-delete requiredMCRProducts.txt  
+    movefile qt_gen_main.exe qtgen.exe
+    movefile readme.txt readme-win.txt
+    delete requiredMCRProducts.txt
 end
 if isunix
-movefile readme.txt readme-x86_64.txt
+    movefile readme.txt readme-x86_64.txt
 end
+cd(curdir);
+movefile ../qt-gen/qtgen.exe .
+movefile ../qt-gen/readme-win.txt .
